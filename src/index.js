6c3cb906853c4cfcdead38e1957f6f93269c9cfc
FarNaya <56936820+FarNaya@users.noreply.github.com>
2022-01-25 08:49:51 -0700

    update

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import "./sass/main.scss";
 import Swal from 'sweetalert2';
 require("@fancyapps/fancybox");
 require("slick-carousel");
+
+
+$( document ).ready(function() {
+
+    
+    $( ".close" ).hide();
+    $( ".alertas-list" ).hide();
+    $( ".open" ).click(function() {
+    $( ".alertas-list" ).slideToggle( "fast", function() {
+    $( ".open" ).hide();
+    $( ".close" ).show();
+    });
+    });
+    $( ".close" ).click(function() {
+    $( ".alertas-list" ).slideToggle( "fast", function() {
+    $( ".close" ).hide();
+    $( ".open" ).show();
+    });
+    });
+ 
+
+    });
